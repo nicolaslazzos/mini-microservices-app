4816637a24abc8d8cb80cacf34462a57318e84f6
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-01-02 20:33:03 -0300

    create and get comments in the comments microservice

--- comments/index.js
+++ comments/index.js
@@ -7,30 +7,32 @@ const app = express();
 // json parser
 app.use(express.json({ extended: false }));
 
-const url = "http://localhost:3000";
+const url = "http://localhost:3001";
 
-app.get("/posts", async (req, res) => {
+app.get("/posts/:id/comments", async (req, res) => {
   try {
-    const result = await axios.get(`${url}/posts`);
+    const postId = req.params.id;
+    const result = await axios.get(`${url}/comments?postId=${postId}`);
     res.send(result.data);
   } catch (e) {
-    console.log("[GET /posts]", e);
+    console.log("[GET /posts/:id/comments]", e);
     res.status(500).send("Internal server error");
   }
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   try {
     const id = randomBytes(4).toString("hex");
-    const { title } = req.body;
-    const result = await axios.post(`${url}/posts`, { id, title });
+    const { content } = req.body;
+    const postId = req.params.id;
+    const result = await axios.post(`${url}/comments`, { id, content, postId });
     res.status(201).send(result.data);
   } catch (e) {
-    console.log("[POST /posts]", e.message);
+    console.log("[POST /posts/:id/comments]", e.message);
     res.status(500).send("Internal server error");
   }
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
