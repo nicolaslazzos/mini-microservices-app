97284207ea1b3ec5d12fa91f2c40e9d53479fe73
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-01-03 22:14:09 -0300

    initializing basic event bus implementation

---
+++ event-bus/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+// json parser
+app.use(express.json({ extended: false }));
+
+const ports = [4000, 4001, 4002];
+
+app.post("/events", async (req, res) => {
+  try {
+    const event = req.body;
+    const requests = [];
+
+    ports.forEach((port) =>
+      requests.push(axios.post(`http://localhost:${port}/events`, event))
+    );
+
+    await axios.all(requests);
+
+    res.send({ status: "Ok" });
+  } catch (e) {
+    console.log("[POST /events]", e.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+const PORT = process.env.PORT || 4005;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
